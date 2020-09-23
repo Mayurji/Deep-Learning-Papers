55199eb05747a5976ad42373ed90587ad1c9fb7a
Mayur_Jain <mayur87545@gmail.com>
2020-09-23 19:03:46 +0530

    Update ReadMe.d

--- Investigate DNN/ReadMe.d
+++ Investigate DNN/ReadMe.d
@@ -1,3 +1,2 @@
-### **Find annotation for paper ["Understanding the Role of Individual Units
-in a Deep Neural Network"](https://arxiv.org/abs/2009.05041)**
+### **Find annotation for paper [Understanding the Role of Individual Units in a Deep Neural Network](https://arxiv.org/abs/2009.05041)**
 
